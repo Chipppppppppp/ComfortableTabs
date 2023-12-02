340da062853dce4c5e4b7ab9a5ebfb275972f3fb
Chippppp <Chippppppppppppppp@gmail.com>
2023-12-03 01:09:48 +0900

    Support both Firefox and Chromium

---
+++ firefox/background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onMessage.addListener((msg, sender) => {
+    chrome.tabs.duplicate(sender.tab.id);
+    chrome.tabs.update(sender.tab.id, { url: msg, active: true });
+    chrome.tabs.move(sender.tab.id, { index: sender.tab.index + 1 });
+});
