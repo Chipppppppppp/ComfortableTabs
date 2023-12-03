f68f93562dc2c79ba59bb1c183153a0d15e980cf
Chippppp <Chippppppppppppppp@gmail.com>
2023-12-03 15:10:08 +0900

    Support shadow-root

--- chromium/background.js
+++ chromium/background.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener((msg, sender) => {
-    chrome.tabs.duplicate(sender.tab.id);
-    chrome.tabs.update(sender.tab.id, { url: msg, active: true });
-    chrome.tabs.move(sender.tab.id, { index: sender.tab.index + 1 });
+    chrome.tabs.duplicate(sender.tab.id, () => {
+        chrome.tabs.update(sender.tab.id, { url: msg, active: true });
+        chrome.tabs.move(sender.tab.id, { index: sender.tab.index + 1 });
+    });
 });
--- chromium/content_script.js
+++ chromium/content_script.js
@@ -1,26 +1,56 @@
-function dfs(doc) {
-    if (doc === null) return;
-    let load = null;
-    doc.addEventListener("click", e => {
-        if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
-        let element = e.target;
-        for (; ; element = element.parentElement) {
-            if (element === null) return;
-            if (element.href !== undefined) break;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        if (load !== null) {
-            clearTimeout(load);
-            load = null;
-            chrome.runtime.sendMessage(element.href);
-            return;
-        }
-        load = setTimeout(() => {
-            load = null;
-            location.href = element.href;
-        }, 300);
-    }, true);
-    for (let iframe of doc.getElementsByTagName("iframe")) dfs(iframe.contentDocument);
+function dfs(win) {
+    if (!win) return;
+
+    try {
+        let load = null, target = null;
+        win.addEventListener("click", e => {
+            if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
+            let element = e.target;
+            for (; ; element = element.parentElement) {
+                if (!element) return;
+                if (element.href !== undefined) break;
+            }
+            e.preventDefault();
+            e.stopPropagation();
+            if (load && target === element) {
+                clearTimeout(load);
+                load = null;
+                target = null;
+                chrome.runtime.sendMessage(element.href);
+                return;
+            }
+            load = setTimeout(() => {
+                load = null;
+                target = null;
+                location.href = element.href;
+            }, 300);
+            target = element;
+        }, true);
+    } catch (_) {
+        return;
+    }
+
+    let doc = win.document;
+    if (!doc) doc = win;
+
+    doc.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
+    doc.querySelectorAll("*").forEach(e => dfs(e.shadowRoot));
+
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(e => {
+                dfs(e.contentDocument);
+                dfs(e.shadowRoot);
+                if (!e.querySelectorAll) return;
+                e.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
+                e.querySelectorAll("*").forEach(e => dfs(e.shadowRoot));
+            });
+        });
+    });
+    observer.observe(doc, {
+        childList: true,
+        subtree: true
+    });
 }
-dfs(document);
+
+dfs(window);
