2576192ae388ad26bf2536d138b3bd6386838fce
Chippppp <Chippppppppppppppp@gmail.com>
2023-12-09 00:03:14 +0900

    Support window.open

--- chromium/background.js
+++ chromium/background.js
@@ -1,6 +1,22 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-    chrome.tabs.duplicate(sender.tab.id, () => {
-        chrome.tabs.update(sender.tab.id, { url: msg, active: true });
-        chrome.tabs.move(sender.tab.id, { index: sender.tab.index + 1 });
-    });
+let newTabs = new Set;
+
+chrome.tabs.onCreated.addListener(tab => {
+    if (tab.openerTabId !== undefined && tab.pendingUrl === undefined && tab.active) {
+        chrome.tabs.update(tab.openerTabId, { active: true });
+        newTabs.add(tab.id);
+    }
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (newTabs.has(tabId)) {
+        newTabs.delete(tabId);
+        if (changeInfo.url.startsWith("http")) {
+            chrome.tabs.remove(tabId);
+            chrome.tabs.sendMessage(tab.openerTabId, changeInfo.url);
+        }
+    }
+});
+
+chrome.runtime.onMessage.addListener((msg, sender) => {
+    chrome.tabs.create({ url: msg, index: sender.tab.index + 1, openerTabId: sender.tab.id }, tab => newTabs.delete(tab.id))
+})
--- chromium/content_script.js
+++ chromium/content_script.js
@@ -2,27 +2,31 @@ function dfs(win) {
     if (!win) return;
 
     try {
-        let load = null, target = null;
+        let load = null, target = null, skip = null;;
         win.addEventListener("click", e => {
             if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
             let element = e.target;
+            if (element === skip) {
+                skip = null;
+                return;
+            }
             for (; ; element = element.parentElement) {
                 if (!element) return;
                 if (element.href !== undefined) break;
             }
+            element.target = "_top";
             e.preventDefault();
             e.stopPropagation();
-            if (load && target === element) {
+            if (element === target) {
                 clearTimeout(load);
-                load = null;
                 target = null;
                 chrome.runtime.sendMessage(element.href);
                 return;
             }
             load = setTimeout(() => {
-                load = null;
+                skip = target;
+                target.dispatchEvent(new MouseEvent("click", e));
                 target = null;
-                location.href = element.href;
             }, 300);
             target = element;
         }, true);
@@ -54,3 +58,29 @@ function dfs(win) {
 }
 
 dfs(window);
+
+
+let div = document.createElement("div");
+div.style.display = "none";
+div.style.position = "fixed";
+div.style.height = "100vh";
+div.style.width = "100vw";
+div.style.top = "0";
+div.style.left = "0";
+div.style.zIndex = 2147483646;
+div.addEventListener("click", () => {
+    clearTimeout(load);
+    div.style.display = "none";
+    chrome.runtime.sendMessage(url);
+});
+document.body.appendChild(div);
+let load = null, url = null;
+
+chrome.runtime.onMessage.addListener(msg => {
+    url = msg;
+    load = setTimeout(() => {
+        div.style.display = "none";
+        location.href = url;
+    }, 300);
+    div.style.display = "block";
+})
