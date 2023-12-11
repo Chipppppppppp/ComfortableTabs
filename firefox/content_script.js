8d508495e1ac44461600145bef52cacbd8b5e741
Chippppp <Chippppppppppppppp@gmail.com>
2023-12-11 10:25:56 +0900

    Fix a bug which happens in reddit.com

--- firefox/content_script.js
+++ firefox/content_script.js
@@ -1,48 +1,61 @@
 function dfs(win) {
     if (!win) return;
 
-    let load = null, target = null, skip = null;
-    win.addEventListener("click", e => {
-        if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
-        let element = e.target;
-        if (element === skip) {
-            skip = null;
-            return;
-        }
-        for (; ; element = element.parentElement) {
-            if (!element) return;
-            if (element.href !== undefined) break;
-        }
-        e.preventDefault();
-        e.stopPropagation();
-        if (element === target) {
-            clearTimeout(load);
-            target = null;
-            chrome.runtime.sendMessage(element.href);
-            return;
-        }
-        load = setTimeout(() => {
-            skip = target;
-            element.target = "_top";
-            target.dispatchEvent(new MouseEvent("click", {
-                clientX: e.clientX,
-                clientY: e.clientY,
-                screenX: e.screenX,
-                screenY: e.screenY
-            }));
-            target = null;
-        }, 300);
-        target = element;
-    }, true);
+    try {
+        let load = null, target = null, skip = null;
+        win.addEventListener("click", e => {
+            if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
+            let element = e.target;
+            if (element === skip) {
+                skip = null;
+                return;
+            }
+            for (; ; element = element.parentElement) {
+                if (!element) return;
+                if (element.href !== undefined) break;
+            }
+            e.preventDefault();
+            e.stopPropagation();
+            if (element === target) {
+                clearTimeout(load);
+                target = null;
+                chrome.runtime.sendMessage(element.href);
+                return;
+            }
+            load = setTimeout(() => {
+                skip = target;
+                element.target = "_top";
+                target.dispatchEvent(new MouseEvent("click", {
+                    clientX: e.clientX,
+                    clientY: e.clientY,
+                    screenX: e.screenX,
+                    screenY: e.screenY
+                }));
+                target = null;
+            }, 300);
+            target = element;
+        });
+    } catch (_) {
+        return;
+    }
 
-    win.document.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
+    let doc = win.document ? win.document : win;
+
+    doc.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
+    doc.querySelectorAll("*").forEach(e => dfs(e.shadowRoot));
 
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
-            mutation.addedNodes.forEach(e => dfs(e.contentWindow));
+            mutation.addedNodes.forEach(e => {
+                dfs(e.contentWindow);
+                dfs(e.shadowRoot);
+                if (!e.querySelectorAll) return;
+                e.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
+                e.querySelectorAll("*").forEach(e => dfs(e.shadowRoot));
+            });
         });
     });
-    observer.observe(win.document, {
+    observer.observe(doc, {
         childList: true,
         subtree: true
     });
