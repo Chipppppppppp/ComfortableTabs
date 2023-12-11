baf21bbb9210a1e9f098a1bba0aa6bd4185896ff
Chippppp <Chippppppppppppppp@gmail.com>
2023-12-11 10:13:23 +0900

    Fix a bug which happens in reddit.com

--- chromium/content_script.js
+++ chromium/content_script.js
@@ -1,63 +1,48 @@
 function dfs(win) {
     if (!win) return;
 
-    try {
-        let load = null, target = null, skip = null;;
-        win.addEventListener("click", e => {
-            if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
-            let element = e.target;
-            if (element === skip) {
-                skip = null;
-                return;
-            }
-            for (; ; element = element.parentElement) {
-                if (!element) return;
-                if (element.href !== undefined) break;
-            }
-            e.preventDefault();
-            e.stopPropagation();
-            if (element === target) {
-                clearTimeout(load);
-                target = null;
-                chrome.runtime.sendMessage(element.href);
-                return;
-            }
-            load = setTimeout(() => {
-                skip = target;
-                element.target = "_top";
-                target.dispatchEvent(new MouseEvent("click", {
-                    clientX: e.clientX,
-                    clientY: e.clientY,
-                    screenX: e.screenX,
-                    screenY: e.screenY
-                }));
-                target = null;
-            }, 300);
-            target = element;
-        }, true);
-    } catch (_) {
-        return;
-    }
+    let load = null, target = null, skip = null;
+    win.addEventListener("click", e => {
+        if (e.ctrlKey || e.shiftKey || e.metaKey || e.altKey || e.button !== 0) return;
+        let element = e.target;
+        if (element === skip) {
+            skip = null;
+            return;
+        }
+        for (; ; element = element.parentElement) {
+            if (!element) return;
+            if (element.href !== undefined) break;
+        }
+        e.preventDefault();
+        e.stopPropagation();
+        if (element === target) {
+            clearTimeout(load);
+            target = null;
+            chrome.runtime.sendMessage(element.href);
+            return;
+        }
+        load = setTimeout(() => {
+            skip = target;
+            element.target = "_top";
+            target.dispatchEvent(new MouseEvent("click", {
+                clientX: e.clientX,
+                clientY: e.clientY,
+                screenX: e.screenX,
+                screenY: e.screenY
+            }));
+            target = null;
+        }, 300);
+        target = element;
+    }, true);
 
-    let doc = win.document;
-    if (!doc) doc = win;
-
-    for (let link of doc.links) link.target = "_top";
-    doc.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
-    doc.querySelectorAll("*").forEach(e => dfs(e.shadowRoot));
+    win.document.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
 
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
-            mutation.addedNodes.forEach(e => {
-                dfs(e.contentDocument);
-                dfs(e.shadowRoot);
-                if (!e.querySelectorAll) return;
-                e.querySelectorAll("iframe").forEach(e => dfs(e.contentWindow));
-                e.querySelectorAll("*").forEach(e => dfs(e.shadowRoot));
-            });
+            mutation.addedNodes.forEach(e => dfs(e.contentWindow));
         });
     });
-    observer.observe(doc, {
+    observer.observe(win.document, {
         childList: true,
         subtree: true
     });
