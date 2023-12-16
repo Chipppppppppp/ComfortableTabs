86bb4fa8ce1361fc86bf353375690a17e9169c07
Chippppp <Chippppppppppppppp@gmail.com>
2023-12-16 19:36:53 +0900

    Fix bugs

--- chromium/background.js
+++ chromium/background.js
@@ -1,20 +1,5 @@
 let newTabs = new Set;
 
-chrome.tabs.onCreated.addListener(tab => {
-    if (tab.openerTabId !== undefined && tab.status === "complete") {
-        chrome.tabs.update(tab.openerTabId, { active: true });
-        newTabs.add(tab.id);
-    }
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url !== undefined && newTabs.has(tabId)) {
-        newTabs.delete(tabId);
-        chrome.tabs.remove(tabId);
-        chrome.tabs.update(tab.openerTabId, { url: changeInfo.url });
-    }
-});
-
 chrome.runtime.onMessage.addListener((msg, sender) => {
-    chrome.tabs.create({ url: msg, index: sender.tab.index + 1 }, tab => chrome.tabs.update(tab.id, { openerTabId: sender.tab.id }));
-})
+    chrome.tabs.create({ url: msg, index: sender.tab.index + 1, openerTabId: sender.tab.id });
+});
--- firefox/background.js
+++ firefox/background.js
@@ -1,24 +1,5 @@
-let newTabs = new Set, newTabsToDelete = new Set;
-
-chrome.tabs.onCreated.addListener(tab => {
-    if (tab.openerTabId !== undefined) {
-        chrome.tabs.update(tab.openerTabId, { active: true });
-        newTabs.add(tab.id);
-    }
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url === undefined) return;
-    if (newTabs.has(tabId)) {
-        newTabs.delete(tabId);
-        if (changeInfo.url === "about:blank") newTabsToDelete.add(tabId);
-        return;
-    } else if (newTabsToDelete.has(tabId)) {
-        chrome.tabs.remove(tabId);
-        chrome.tabs.update(tab.openerTabId, { url: changeInfo.url });
-    }
-});
+let newTabs = new Set;
 
 chrome.runtime.onMessage.addListener((msg, sender) => {
-    chrome.tabs.create({ url: msg, index: sender.tab.index + 1 }, tab => chrome.tabs.update(tab.id, { openerTabId: sender.tab.id }));
-})
+    chrome.tabs.create({ url: msg, index: sender.tab.index + 1, openerTabId: sender.tab.id });
+});
